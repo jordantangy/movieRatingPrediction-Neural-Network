5d0e8404fc971683a8b056e0c40697168b8d18a9
jordantangy@gmail.com <jordantangy@gmail.com>
2020-12-20 00:11:44 +0200

    update

--- recommender.py
+++ recommender.py
@@ -1,10 +1,11 @@
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import math
 import pandas as pd
 
-#retrieve data for specific user(userId, movieId,rating) and separating training set and test set
+# retrieve data for specific user(userId, movieId,rating) and separating training set and test set
 
+tf.disable_v2_behavior()
 df = pd.read_csv('ratings.csv', usecols = ['userId','movieId','rating'], low_memory = False)
 temp = df.to_numpy()
 temp_mat = []
@@ -36,61 +37,62 @@ for i in y:
     temp_test.append(row)
 test_data = np.array(temp_test)
 
-# creating neural network
-print("started")
-features = 1128
-df = pd.read_csv('genome-scores.csv', usecols=['movieId', 'relevance'], low_memory=False)
-relevance = df.to_numpy()
-x = tf.placeholder(tf.float32, shape=[1, features])
-y_ = tf.placeholder(tf.float32, [1])
-W = tf.Variable(tf.zeros([features, 1]))
-b = tf.Variable(tf.zeros([1]))
-y = tf.matmul(x, W) + b
-loss = tf.reduce_mean(tf.pow(y - y_, 2))
-update = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-for i in range(len(training_data)):
-    movieId = training_data[i][1]
-    data_y = [training_data[i][2]]
-    temp_mat = []
-    np.set_printoptions(suppress=True)
-    for j in range(len(relevance)):
-        if (relevance[j][0] == movieId):
-            temp_mat.append(relevance[j][1])
-    data_x = [np.array(temp_mat)]
-    print(data_x)
-    for k in range(0, 1000000):
-        sess.run(update, feed_dict={x: data_x, y_: data_y})
-        if (k % 10000 == 0):
-            print('Iteration:', k, ' W:', sess.run(W), ' b:', sess.run(b), ' loss:',
-                  loss.eval(session=sess, feed_dict={x: data_x, y_: data_y}))
-            print("prediction:", np.matmul(data_x, sess.run(W)) + sess.run(b))
+#creating neural network and training
 
-# TODO testing the NN
-
+print("started training...")
 features = 1128
 df = pd.read_csv('genome-scores.csv', usecols = ['movieId','relevance'], low_memory = False)
 relevance = df.to_numpy()
 x = tf.placeholder(tf.float32,  shape=[1,features])
 y_ = tf.placeholder(tf.float32, [1])
+W = tf.Variable(tf.zeros([features,1]))
 b = tf.Variable(tf.zeros([1]))
 y = tf.matmul(x,W) + b
 loss = tf.reduce_mean(tf.pow(y - y_, 2))
+update = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-for i in range(len(test_data)):
+for i in range(len(training_data)):
     print("----------------------------------------------------------------------")
-    movieId = test_data[i][1]
-    data_y = [test_data[i][2]]
+    print("Movie : ", i , "/", len(training_data))
+    movieId = training_data[i][1]
+    data_y = [training_data[i][2]]
     temp_mat = []
     np.set_printoptions(suppress=True)
     for j in range(len(relevance)):
         if(relevance[j][0] ==movieId):
             temp_mat.append(relevance[j][1])
     data_x =[np.array(temp_mat)]
+    for k in range(0,50000):
+        sess.run(update, feed_dict = {x:data_x, y_:data_y})
+        w_updated = W.eval(sess)
+        b_updated = b.eval(sess)
+        if(k % 10000 == 0):
+                print('Iteration:' , k , ' W:' , sess.run(W), ' b:' , sess.run(b), ' loss:', loss.eval(session=sess, feed_dict = {x:data_x, y_:data_y}))
+
+
+
+# testing the NN
+
+
+print("starting testing...")
+
+
+for i in range(len(test_data)):
+    print("----------------------------------------------------------------------")
+    print("Movie : ", i, "/", len(test_data))
+    movieId = test_data[i][1]
+    actual = test_data[i][2]
+    # data_y = [test_data[i][2]]
+    temp_mat = []
+    np.set_printoptions(suppress=True)
+    j = range(0, len(relevance))
+    for t in j:
+        if (relevance[t][0] == movieId):
+            temp_mat.append(relevance[t][1])
+    data_x = [np.array(temp_mat)]
     print(data_x)
-    sess.run(y, feed_dict = {x:data_x, y_:data_y})
-    print('Iteration:' ,' W:' , sess.run(W), ' b:' , sess.run(b), ' loss:', loss.eval(session=sess, feed_dict = {x:data_x, y_:data_y}))
-    print("prediction:",np.matmul(data_x,sess.run(W)) + sess.run(b))
+    pred = np.matmul(data_x, w_updated) + b_updated
+    print("For Movie : ", i, "/", len(test_data), "the prediction is:", pred, "and the actual rating is", actual)
+
 
